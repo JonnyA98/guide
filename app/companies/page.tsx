ffe26ded790111d6eddb90d88b181c7a603a338d
BigBoiJonny <jonny98.0@outlook.com>
2024-10-27 10:25:42 +0000

    disable eslint

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react'
-import { Search, Filter, Star, ThumbsUp, ThumbsDown, TrendingUp, Users, BarChart2 } from 'lucide-react'
+import { Search, Filter,  ThumbsUp, ThumbsDown, TrendingUp, Users, BarChart2 } from 'lucide-react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
